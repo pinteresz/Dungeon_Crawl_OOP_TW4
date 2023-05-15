1fd9afe03c3e91bab86ca710312dd67fb5a4a3f8
pinteresz <pinteresz@gmail.com>
2023-05-15 14:10:20 +0200

    feat: add wall

---
+++ Tiles/Wall.cs
@@ -0,0 +1,24 @@
+﻿using SadConsole;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SadRogue.Primitives;
+
+
+namespace DungeonCrawl.Tiles
+{
+    public class Wall : GameObject
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="position"></param>
+        /// <param name="hostingSurface"></param>
+        public Wall(Point position, IScreenSurface hostingSurface)
+            : base(new ColoredGlyph(Color.LightBlue, Color.LightBlue, 'W'), position, hostingSurface)
+        {
+        }
+    }
+}
